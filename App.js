bcda3cf152ac74ca76a34e950866cdd11777d95c
SANJUAN\samyak.jain <samyak.jain@avalara.com>
2023-01-06 19:38:52 +0530

    Knowledge about Bundlers

--- App.js
+++ App.js
@@ -1,6 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
+
+/*  Bundlers
+Parcel needs some space to do all of this and it takes via .parcel-cache
+DO not Add .parcel-cache on repository
+1. HMR - Hot module reloading using file watcher algorithm (C++)
+2. Minify Code, chunking, bundling
+3. Tree shaking - Remove unwanted code like logs and chunking
+4. Server for hosting locally
+5. Different Dev and Prod build
+6. Image Optimizations and compressions
+7. Caching while development
+8. Backward compatibilty (using polyfills)
+9. Can enable https on local using parcel
+10. Provide source map for easy debugging
+11. Manages Port Numbers
+12. Consistent Hashing Algorithm for caching and bundling
+13. Parcel is a Zero Config Bundler
+*/
 const heading = React.createElement(
     "h1", // Tag
     {
