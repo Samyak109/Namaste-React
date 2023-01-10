3ed48f1cd4d86af92457c17f33636e1f6b00973c
SANJUAN\samyak.jain <samyak.jain@avalara.com>
2023-01-10 20:38:03 +0530

    Introducing JSX

--- App.js
+++ App.js
@@ -1,29 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const heading = React.createElement(
-  "h1", // Tag
-  {
-    // we pass attributes here
-    id: "title",
-    key: "h1", // Passing key
-  },
-  "Namaste From React" // Children/Inner Body
+const heading = (
+  <h1 id="title" key={"h1"}>
+    Namaste From React
+  </h1>
 );
-const heading2 = React.createElement(
-  "h2",
-  {
-    id: "title",
-    key: "h2",
-  },
-  "Namaste From React h2"
+
+const heading2 = (
+  <h2 id="title2" key={"h2"}>
+    Namaste From React h2
+  </h2>
 );
-const container = React.createElement(
-  "div",
-  {
-    id: "container",
-  },
-  [heading, heading2] // These are siblings so for efficient rendering we use keys so that react don't have to re render children with same key
+
+const container = (
+  <div id="container">
+    {heading}
+    {heading2}
+  </div>
 );
 /* In react we need to assign root to something so it renders in that root*/
 const root = ReactDOM.createRoot(document.getElementById("root"));
