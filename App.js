df0b7ed552e44307411c6814a9f756a2a662cbca
SANJUAN\samyak.jain <samyak.jain@avalara.com>
2023-01-11 18:22:09 +0530

    Superpower of JSX and common doubts

--- App.js
+++ App.js
@@ -1,6 +1,34 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+/*
+Why do multiple JSX tags need to be wrapped
+Ans - JSX looks like HTML, but under the hood it is transformed into plain JavaScript objects. You can’t return two objects from a function without wrapping them into an array. 
+This explains why you also can’t return two JSX tags without wrapping them into another tag or a Fragment.
+<> </> => It is fragment and do not leave trace on DOM
 
+Why Camelcase in JSX ? 
+JSX turns into JavaScript and attributes written in JSX become keys of JavaScript objects. 
+In your own components, you will often want to read those attributes into variables. 
+But JavaScript has limitations on variable names. 
+For example, their names can’t contain dashes or be reserved words like class.
+For historical reasons, aria-* and data-* attributes are written as in HTML with dashes.
+
+HTML to JSX Converter
+https://transform.tools/html-to-jsx
+
+Passing strings, numbers, JSExpressions we use single curly braces {}
+You can even pass objects in JSX using {{}} 
+Used for writing inline CSS 
+style={{
+      backgroundColor: 'black',
+      color: 'pink'
+    }}
+The next time you see {{ and }} in JSX, know that it’s nothing more than an object inside the JSX curlies!
+
+Inline style properties are written in camelCase. 
+For example, HTML <ul style="background-color: black"> 
+would be written as <ul style={{ backgroundColor: 'black' }}>  in your component.
+*/
 const heading = (
   <h1 id="title" key={"h1"}>
     Namaste From React
