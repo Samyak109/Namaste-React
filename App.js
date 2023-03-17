3037e746f3916e87135dc034388d6acd5a2131b0
SANJUAN\samyak.jain <samyak.jain@avalara.com>
2023-03-17 18:58:39 +0530

    Why did I remove import React ?, Grouping Operator Role

--- App.js
+++ App.js
@@ -1,41 +1,13 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
-/*
-Why do multiple JSX tags need to be wrapped
-Ans - JSX looks like HTML, but under the hood it is transformed into plain JavaScript objects. You can’t return two objects from a function without wrapping them into an array. 
-This explains why you also can’t return two JSX tags without wrapping them into another tag or a Fragment.
-<> </> => It is fragment and do not leave trace on DOM
-
-Why Camelcase in JSX ? 
-JSX turns into JavaScript and attributes written in JSX become keys of JavaScript objects. 
-In your own components, you will often want to read those attributes into variables. 
-But JavaScript has limitations on variable names. 
-For example, their names can’t contain dashes or be reserved words like class.
-For historical reasons, aria-* and data-* attributes are written as in HTML with dashes.
-
-HTML to JSX Converter
-https://transform.tools/html-to-jsx
-
-Passing strings, numbers, JSExpressions we use single curly braces {}
-You can even pass objects in JSX using {{}} 
-Used for writing inline CSS 
-style={{
-      backgroundColor: 'black',
-      color: 'pink'
-    }}
-The next time you see {{ and }} in JSX, know that it’s nothing more than an object inside the JSX curlies!
-
-Inline style properties are written in camelCase. 
-For example, HTML <ul style="background-color: black"> 
-would be written as <ul style={{ backgroundColor: 'black' }}>  in your component.
-*/
+/* With the new JSX transform from react 17, you can use JSX without importing React. 
+https://legacy.reactjs.org/blog/2020/09/22/introducing-the-new-jsx-transform.html#whats-a-jsx-transform*/
 const heading = (
   <h1 id="title" key={"h1"}>
     Namaste From React
   </h1>
 );
 
-const Title = () => (
+const Title = () => ( // Why we insert Parenthesis here ? Short Answer => we need () to return an object from arrow function. Long -> Read about Grouping operator 
   <div>
     <h1>Namaste React from functional component Title</h1>
   </div>
