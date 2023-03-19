367b90826bf472839f0fd47fd90239f0ead4a657
SANJUAN\samyak.jain <samyak.jain@avalara.com>
2023-03-19 11:21:28 +0530

    Basic Completed Header

--- App.js
+++ App.js
@@ -1,16 +1,54 @@
 import ReactDOM from "react-dom/client";
 
-const HeaderComponent = () => (
-  <div style={{ border: "1px solid"}}>
-    {/* This is a Comment */}
-    {/* JSX - Not Mandantory 
-        ES 6 - Not Mandatory
-    */}
-    <h1>Starting Project</h1>   
-  </div>
+const AppLayout = () => (
+  <>
+    <Header />
+    <Body />
+    <Footer />
+  </>
 );
+
+const Logo = () => (
+  <a href="/">
+  <img className="logo" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQOOQowG2cFpBqxhqnpE5TGf-bNBINTkzf5DBlOzjxXOT6KiHrApDGKkILMi-LnkjSUHmg&usqp=CAU" alt="logo" />
+  </a>
+);
+
+const NavLinks = () => (
+  <>
+  <ul className="nav-items">
+    <li>Home</li>
+    <li>About</li>
+    <li>Help</li>
+    <li>Cart</li>
+  </ul>
+  </>
+);
+
+const UserProfile = () => (
+  <i className="fa fa-cloud icon">Samyak</i>
+);
+
+const Cart = () => (
+  <>
+  <i className="fa fa-cart-plus">Cart</i>
+  </>
+);
+const Header = () => (
+<div className="header">
+  <Logo />
+  <NavLinks />
+  <UserProfile />
+  <Cart />
+</div>
+);
+
+
+const Body = () => <h1>Body</h1>;
+
+const Footer = () => <h1>Footer</h1>;
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 // passing react element to root, render takes react element and modifies DOM
-root.render(<HeaderComponent />);
+root.render(<AppLayout />);
