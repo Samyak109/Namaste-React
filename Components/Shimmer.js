b864ab08821c7a44f90f45427eab7260762c1a75
SANJUAN\samyak.jain <samyak.jain@avalara.com>
2023-04-14 10:53:39 +0530

    Add Shimmer UI

---
+++ Components/Shimmer.js
@@ -0,0 +1,6 @@
+import { ShimmerSimpleGallery } from "react-shimmer-effects-18";
+
+const ShimmerUI = () => (
+    <ShimmerSimpleGallery row={4} col={4} card frameHeight={600} />
+);
+export default ShimmerUI;
